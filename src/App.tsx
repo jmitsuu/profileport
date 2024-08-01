4caea1896a0a67a273c29175d3e5ca146e76d594
unknown <jmiguelafonso@hotmail.com>
2024-07-31 23:59:20 -0300

    feature:create new card

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Footer } from './components/Footer';
 import { Container } from './layout/Container';
 function App() {
   return (
-    <main className=" min-h-screen bg-black dark:bg-slate-200 ">
+    <main className=" min-h-screen bg-black dark:bg-slate-100 ">
       <div className=" h-full bg-tecHeavy dark:bg-white flex flex-col relative  border-x-[0.1em] border-tecMedium md:w-[1300px] mx-auto min-h-screen  ">
         <Header />
         <Container>
