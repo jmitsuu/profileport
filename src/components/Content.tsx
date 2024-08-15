13f9e3eec19fd2e1100a297da05bc1966354592c
jmitsuu <jmiguelafonso@hotmail.com>
2024-08-15 17:15:41 -0300

    actions/deploy

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -8,7 +8,7 @@ export function Content({
 }: typeContent) {
   return (
     <main className="md:w-[980px] w-full  md:mb-10">
-      <h1 className="text-white  my-10  dark:text-tecHeavy font-bold md:text-[3rem] text-3xl text-pretty ">
+      <h1 className="text-yellow-500  my-10  dark:text-tecHeavy font-bold md:text-[3rem] text-3xl text-pretty ">
         {title}
       </h1>
       <p className="text-tecLight dark:text-tecMedium mt-5 md:text-2xl text-xl">
