8d0fd495a6d560787bc7de53bb760757750da2ef
unknown <jmiguelafonso@hotmail.com>
2025-02-12 11:48:19 -0300

    feat:change file download

--- src/components/Download.tsx
+++ src/components/Download.tsx
@@ -3,7 +3,7 @@ import { IoArrowDown } from 'react-icons/io5';
 export function Download() {
   return (
     <a
-      href="https://drive.google.com/uc?export=download&id=1JOx8FiY3A2Lnyllewh_3Jq-bViHedTn3"
+      href="https://drive.google.com/uc?export=download&id=1zTqwrKFTebBTvG0HQ6mzLLkMR4wJaeYJ"
       className="md:mx-0 mx-auto w-56 text-[#38bdf8]  dark:text-tecHeavy  py-2 rounded-lg flex items-center justify-center gap-2  md:mt-24 mt-8 border border-[#38bdf8] transition-all hover:bg-tecMediumLight/10 dark:bg-tecLight uppercase "
     >
       Baixar Currículo <IoArrowDown />
